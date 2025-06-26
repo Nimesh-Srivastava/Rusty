8305ffc00d1240f33730fca6d069f4f383c1a080
Nimesh Srivastava <snimesh412@gmail.com>
2025-06-26 19:22:31 +0530

    Init paging

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,20 @@
 
 use core::panic::PanicInfo;
 use rusty::println;
+use bootloader::{BootInfo, entry_point};
 
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello world{}", "!");
 
     rusty::init();
     
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_page_table, _) = Cr3::read();
+    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+
     #[cfg(test)]
     test_main();
 
