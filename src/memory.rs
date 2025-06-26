1b2ff84bc96edafb56985712e868f5d3a094a472
Nimesh Srivastava <snimesh412@gmail.com>
2025-06-27 02:08:19 +0530

    New mapping in page tables

--- src/memory.rs
+++ src/memory.rs
@@ -1,5 +1,4 @@
-use x86_64::{structures::paging::PageTable, VirtAddr,};
-use x86_64::PhysAddr;
+use x86_64::{structures::paging::{Page, PhysFrame, Mapper, Size4KiB, FrameAllocator, PageTable}, VirtAddr, PhysAddr};
 
 pub unsafe fn translate_addr(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
     translate_addr_inner(addr, physical_memory_offset)
@@ -33,14 +32,47 @@ fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Opt
     Some(frame.start_address() + u64::from(addr.page_offset()))
 }
 
-//pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
-//    use x86_64::registers::control::Cr3;
-//
-//    let (level_4_table_frame, _) = Cr3::read();
-//
-//    let phys = level_4_table_frame.start_address();
-//    let virt = physical_memory_offset + phys.as_u64();
-//    let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
-//
-//    unsafe {&mut *page_table_ptr}
-//}
+use x86_64::structures::paging::OffsetPageTable;
+
+pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+    unsafe {
+        let level_4_table = active_level_4_table(physical_memory_offset);
+        OffsetPageTable::new(level_4_table, physical_memory_offset)
+    }
+}
+
+unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_table_frame, _) = Cr3::read();
+
+    let phys = level_4_table_frame.start_address();
+    let virt = physical_memory_offset + phys.as_u64();
+    let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
+
+    unsafe {&mut *page_table_ptr}
+}
+
+pub fn create_example_mapping(
+    page: Page,
+    mapper: &mut OffsetPageTable,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) {
+    use x86_64::structures::paging::PageTableFlags as Flags;
+
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+    let flags = Flags::PRESENT | Flags::WRITABLE;
+
+    let map_to_result = unsafe {
+        mapper.map_to(page, frame, flags, frame_allocator)
+    };
+    map_to_result.expect("map_to failed").flush();
+}
+
+pub struct EmptyFrameAllocator;
+
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        None
+    }
+}
