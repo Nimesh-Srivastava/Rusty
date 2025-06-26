5b86316be1ba4c4b5c5d94104ac4c9168890d587
Nimesh Srivastava <snimesh412@gmail.com>
2025-06-26 18:05:13 +0530

    Fix deadlocks and race condition

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,10 @@ pub extern "C" fn _start() -> ! {
     test_main();
 
     println!("It did not crash");
-    loop{}
+    loop{
+        use rusty::print;
+        print!("-");
+    }
 }
 
 #[cfg(not(test))]
