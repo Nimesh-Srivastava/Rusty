514167c6930ccdb53352fb19ff57f1fd0a129c9a
Nimesh Srivastava <snimesh412@gmail.com>
2025-06-26 17:39:19 +0530

    Initialize hardware inturrepts

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -3,6 +3,14 @@ use crate::println;
 use lazy_static::lazy_static;
 use crate::gdt;
 
+use pic8259::ChainedPics;
+use spin;
+
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+pub static PICS: spin::Mutex<ChainedPics> = spin::Mutex::new(unsafe {ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET)});
+
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
--- src/lib.rs
+++ src/lib.rs
@@ -70,7 +70,10 @@ pub extern "C" fn _start() -> ! {
 
 pub fn init() {
     gdt::init();
-   interrupts::init_idt(); 
+    interrupts::init_idt(); 
+
+    unsafe{ interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 }
 
 #[cfg(test)]
