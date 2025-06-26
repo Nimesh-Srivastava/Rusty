05fbd296ce31c7684a810ea672923a0c1807bcf2
Nimesh Srivastava <snimesh412@gmail.com>
2025-06-26 17:31:44 +0530

    Added integration test for stack overflow

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,7 @@ pub extern "C" fn _start() -> ! {
     println!("Hello world{}", "!");
 
     rusty::init();
-
-    //x86_64::instructions::interrupts::int3();
     
-    fn stack_overflow() {
-        stack_overflow();
-    }
-
-    stack_overflow();
-
     #[cfg(test)]
     test_main();
 
